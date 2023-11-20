adb80489cbaef1d6aa69b316a44fbd4babd6e2a4
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-11-19 21:23:58 -0300

    new portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburguer = document.querySelector('.hamburguer')
+const navMenu = document.querySelector('.nav-menu')
+
+hamburguer.addEventListener('click', function(){
+    hamburguer.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+const changeTheme = document.querySelector('#change-theme')
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark')
+}
+
+changeTheme.addEventListener("change", function() {
+    toggleDarkMode()
+})
+
